8190527abaebbf81806a343882ddcfc3bb9488df
CHAN Ho Long <roychanho@gmail.com>
2023-02-03 15:45:36 +0800

    update

--- app/issues/[issueId]/page.js
+++ app/issues/[issueId]/page.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { wait } from "../../../util/time";
 import IssueDetail from "./IssueDetails";
 
 async function IssueDetailsPage({ params }) {
@@ -6,7 +7,13 @@ async function IssueDetailsPage({ params }) {
   const issue = await prisma.issue.findFirst({
     where: { id: +params.issueId },
   });
+
+  //   await wait(3);
   
+  if (!issue) {
+    throw new Error("Issue not found!");
+  }
+
   return <IssueDetail issue={issue} />;
 }
 
